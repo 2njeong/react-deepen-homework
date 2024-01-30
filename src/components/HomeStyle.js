33d9bc2590ef5f935fc4593ef85521ec9bfedca8
injeong Hwang <twilight9758@gmail.com>
2024-01-30 14:02:03 +0900

    HomeStyle 컴포넌트 분리

---
+++ src/components/HomeStyle.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const FanLetterStContainer = styled.div`
+  background-color: beige;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-content: stretch;
+
+  width: 600px;
+  height: 200px;
+  margin: 50px auto 10px auto;
+  padding: 10px;
+  border-radius: 10px;
+`;
+
+export const FanletterStBox = styled.div`
+  /* background-color: aquamarine; */
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 30px;
+  margin: 5px auto 5px auto;
+  padding: 5px;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const InputStBox = styled.input`
+  width: 500px;
+  height: 20px;
+  border-radius: 5px;
+`;
+
+export const WhoStBox = styled.div`
+  /* background-color: pink; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 20px;
+  width: 590px;
+  font-weight: bold;
+  font-size: 15px;
+  margin: 10px;
+  /* padding-left: 15px; */
+`;
+
+export const SelectStBox = styled.select`
+  width: 300px;
+  height: 25px;
+`;
+
+export const SubmitBtn = styled.button`
+  width: 150px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  font-size: 15px;
+  margin: 10px 10px 10px auto;
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,69 +1,13 @@
 import React from "react";
 import { useState } from "react";
-import styled from "styled-components";
-
-const FanLetterStContainer = styled.div`
-  background-color: beige;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  justify-content: center;
-  align-content: stretch;
-
-  width: 600px;
-  height: 200px;
-  margin: 50px auto 10px auto;
-  padding: 10px;
-  border-radius: 10px;
-`;
-
-const FanletterStBox = styled.div`
-  /* background-color: aquamarine; */
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  height: 30px;
-  margin: 5px auto 5px auto;
-  padding: 5px;
-  font-size: 15px;
-  font-weight: bold;
-`;
-
-const InputStBox = styled.input`
-  width: 500px;
-  height: 20px;
-  border-radius: 5px;
-`;
-
-const WhoStBox = styled.div`
-  /* background-color: pink; */
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  height: 20px;
-  width: 590px;
-  font-weight: bold;
-  font-size: 15px;
-  margin: 10px;
-  /* padding-left: 15px; */
-`;
-
-const SelectStBox = styled.select`
-  width: 300px;
-  height: 25px;
-`;
-
-const SubmitBtn = styled.button`
-  width: 150px;
-  height: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: bold;
-  font-size: 15px;
-  margin: 10px 10px 10px auto;
-`;
+import {
+  FanLetterStContainer,
+  FanletterStBox,
+  InputStBox,
+  WhoStBox,
+  SelectStBox,
+  SubmitBtn,
+} from "../components/HomeStyle";
 
 function Home() {
   const [nickName, setNicname] = useState("");
