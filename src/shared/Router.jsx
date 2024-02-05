82def6ee1f6f80918812e133c11299b07566e0f7
injeong Hwang <twilight9758@gmail.com>
2024-02-05 13:27:42 +0900

    리팩토링

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,8 +1,6 @@
-// import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import Detail from "../pages/Detail.jsx";
-// import { fakeData } from "./data.js";
 import GlobalStyle from "../GlobalStyle.jsx";
 
 const Router = () => {
